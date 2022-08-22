b2939100f89a958b109809be6b5edc19adc3a573
Mohanish Chavan <beingmohanish15@gmail.com>
2022-08-22 18:10:09 +0530

    update app

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCvBUjMpcFu3w283wMfVyRa_LXap1ZG0Eo",
-  authDomain: "news-feed-management.firebaseapp.com",
-  projectId: "news-feed-management",
-  storageBucket: "news-feed-management.appspot.com",
-  messagingSenderId: "417767876950",
-  appId: "1:417767876950:web:386f55f23dc3d3e5b80ef7",
-  measurementId: "G-5Z37PZPM7W",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
